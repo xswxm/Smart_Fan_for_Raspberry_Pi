cbd5f75d42cae4dbd545e250c30e57a9e6c57e88
Adon <hzxswxm@gmail.com>
2017-09-08 16:01:21 +1000

    PWM support
    1. Added pwm support;
    2. Added safely exit for both scripts;
    3. Updated readme.

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 A script designed to manage your Raspberry Pi's fan according to the CPU temperature changes.
 
+### Main
+There are two different versions, the ordinary one named 'fan.py' does not support pwm feature so it can only turn on or turn off the fan.
+The PWM version, which is 'fan_pwm.py', is functional with pwm adjustment.
+
+### Problem
+Both scripts works appropriately.
+For the pwm version, you may have to increse its initial speed if the fan does not spin.
+Based on the quality of your fan, it could make more noise when you are using the pwm version.
+
 ### Modify your ordinary fan with an appropriate transistor
 <img src="https://github.com/xswxm/Smart_Fan_for_Raspberry_Pi/blob/master/demo.png?raw=true" 
 alt="Demo" width="249" height="224" border="10" />
@@ -9,16 +18,33 @@ Install additional modules
 ```sh
 sudo apt-get update
 sudo apt-get install python python-pip
+# Install requirements
+sudo apt-get install RPi.GPIO, pigpio
 ```
 
-### How to Use
+### How to Use (without PWM)
 ```sh
 # Check help documents
 sudo python fan.py -h
 # Run with default settings
 sudo python fan.py
+# Run in the background
+sudo nohup python fan.py &
 # Check CPU temperature every 2 seconds abd turn on fan on pin 24 if temperature is higher than 50 celsius
 sudo python fan.py -t 50 -i 2 -p 24
+```
+
+### How to Use (with PWM)
+```sh
+# Check help documents
+sudo python fan_pwm.py -h
+# Run with default settings
+sudo python fan_pwm.py
+# Run in the background
+sudo nohup python fan_pwm.py &
+# Check CPU temperature every 2 seconds abd turn on fan on pin 24 if temperature is higher than 50 celsius,
+# and increse the fan speed by 10% when the temperature increses by 1 celisus
+sudo python fan_pwm.py -t 50 -i 2 -p 24 -s 10
 ```
 
 License
--- fan_pwm.py
+++ fan_pwm.py
@@ -8,28 +8,38 @@ This script is designed to manage your Raspberry Pi's fan according to the CPU t
 
 '''
 
-import RPi.GPIO as GPIO
+import pigpio
 import os, time
 
 # Parse command line arguments
 import argparse
 parser = argparse.ArgumentParser(description='Manage your Raspberry Pi Fan intelligently!')
+parser.add_argument('-d', '--duty_cycle_init', type=int, 
+  help='Initial duty cycle out of 100. default=40', default=40)
+parser.add_argument('-f', '--frequency', type=int, 
+  help='PWM frequency. default=50', default=50)
 parser.add_argument('-i', '--interval', type=int, 
   help='The interval of each CPU temperature check (in seconds). default=5', default=5)
 parser.add_argument('-p', '--pin', type=int, 
   help='Pin number of the Fan. default=24', default=24)
+parser.add_argument('-s', '--step', type=float, 
+  help='Speed change of the fan out of 100 percent. default=6', default=6)
 parser.add_argument('-t', '--temp_limit', type=int, 
-  help='Fan will be turned on once the temperature is higher than this value (in celsius). default=60', default=60)
+  help='Fan will be turned on once the temperature is higher than this value (in celsius). default=50', default=50)
 args = parser.parse_args()
 
+duty_cycle = 0
+duty_cycle_init = args.duty_cycle_init
+frequency = args.frequency
 interval = args.interval
 pin = args.pin
-temp = 0
+step = float(args.step)/1000
+temp = 0    # Initialize temperature
 temp_limit = args.temp_limit * 1000
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(pin, GPIO.OUT)
+pi = pigpio.pi()
+pi.set_PWM_frequency(pin, frequency)
+pi.set_PWM_range(pin, 100)    # Set range to 100
 
 try:
   while True:
@@ -38,11 +48,13 @@ try:
       temp = int(file.read())
     # Turn on the fan if temp is larger than the limit value
     if temp > temp_limit:
-      GPIO.output(pin, GPIO.HIGH)
-    # Turn off the fan if temp is smaller than the value - 2000
-    elif  temp < temp_limit-5000:
-      GPIO.output(pin, GPIO.LOW)
+      duty_cycle = duty_cycle_init+(temp-temp_limit)*step
+      if duty_cycle > 100: duty_cycle = 100
+    # Turn off the fan if temp is smaller than the value
+    elif  temp < temp_limit:
+      duty_cycle = 0
+    pi.set_PWM_dutycycle(pin, duty_cycle)
     # Sleep for few seconds
     time.sleep(interval)
 except KeyboardInterrupt:
-  GPIO.cleanup()
+  pi.set_servo_pulsewidth(pin, 0)
